c9fd5dfd66c584fd8f550a798cd7299bf300eb52
KHAOULA <ezaoui.khaoula@gmail.com>
2022-04-06 11:37:50 +0000

    Added the git deployment script

--- Gestion_GVT/settings.py
+++ Gestion_GVT/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(c0c4p9f*a%s0f7abmjuro&84u6j)*lzj2zkw4b1xi4$^u0ol7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['vtgestion.herokuapp.com','127.0.0.1']
 
@@ -141,7 +141,7 @@ USE_TZ = True
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
